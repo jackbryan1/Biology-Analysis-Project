a1141a354d8526978b5055c7dd1ec23b84cd9563
b9015402 <b9015402@newcastle.ac.uk>
2019-10-16 11:09:36 +0100

    Added average length

--- uniplot/analysis.py
+++ uniplot/analysis.py
@@ -1,4 +1,4 @@
 def average_len(records):
         """Returns the average len for records"""
         RecordLength = [len(i) for i in records]
-        return float(sum(RecordLength)) / len(RecordLength)
+        return sum(RecordLength) / len(RecordLength)
