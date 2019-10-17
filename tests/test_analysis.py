7de1cc1817099deaf655e89a76d28d760fb193e0
b9015402 <b9015402@newcastle.ac.uk>
2019-10-17 10:05:18 +0100

    Added testing

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,4 +1,13 @@
 import pytest
+import uniplot.analysis
+import uniplot.parse
+
+TEST_UNIPROT="./resources/uniprot_sprot_small.xml.gz"
 
 def test_hello_world():
     assert True
+
+def test_average():
+    assert uniplot.analysis.average_len(
+        uniplot.parse.uniprot_seqrecords(TEST_UNIPROT)
+    ) == 550.2026350208952
